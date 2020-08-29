fae654140f82d98c8c621859df7cc50009bdaabe
Jack Sanders <guitarbreakerjs@gmail.com>
2020-08-29 07:16:05 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def meal_choice(veg1,veg2,protein = 'tofu')
-  puts "What a nutritious meal!"
+  print "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 binding.pry
