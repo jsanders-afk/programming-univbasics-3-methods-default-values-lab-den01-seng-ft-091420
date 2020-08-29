4f97c8b66685714d79c8ae443281a03f8d9320b3
Jack Sanders <guitarbreakerjs@gmail.com>
2020-08-29 07:28:02 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,7 @@
-# Your code here
-# For output purposes, use "puts" instead of "print" or "p"
+require 'pry'
+def meal_choice(veg1,veg2,protein = 'tofu')
+  puts "What a nutritious meal!"
+  message2 = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts message2
+  return message2
+end
